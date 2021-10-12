2c441b554abaa6b23448c6de4c1cf919498f3646
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2021-10-12 16:27:10 +0100

    Chapter 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,17 @@
 require 'bike'
 
 class DockingStation
+  attr_reader :bike
+
   def release_bike
-    Bike.new
+    bike = Bike.new
   end
+
+  def dock(bike)
+   @bike = bike
+  end 
+
+  # def bike 
+  #   @bike
+  # end 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,4 +8,15 @@ describe DockingStation do
     bike = station.release_bike
     expect(bike).to be_working
   end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it {  is_expected.to respond_to(:bike) }
+
+  it 'docks a bike' do 
+    station = DockingStation.new
+    bike = Bike.new
+    expect(station.dock(bike)).to eq bike
+  end 
+
+  
 end
